9ba3877260bb5c09fab5cc2ee46f148ee315b0fd
walon <walon@code-philosophy.com>
2026-01-15 22:22:32 +0800

    new: add test for System.RuntimeTypeHandle::IsByRefLike

---
+++ src/tests/managed/CorlibTests/InternalCall/TC_System_RuntimeTypeHandle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CorlibTests.InternalCall
+{
+    internal class TC_System_RuntimeTypeHandle : GeneralTestCaseBase
+    {
+        [UnitTest]
+        public void ByRefLike()
+        {
+            var t = typeof(int);
+            Assert.False(t.IsByRefLike);
+        }
+
+        [UnitTest]
+        public void ByRefLike_Span()
+        {
+            var t = typeof(Span<int>);
+            Assert.True(t.IsByRefLike);
+        }
+    }
+}
